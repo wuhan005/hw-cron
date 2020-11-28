d9191cf5a3b8d868aede757d5a794a65c7f74149
E99p1ant <i@github.red>
2020-11-29 02:27:58 +0800

    alert: use webhook to send alert

--- fanya/homework.go
+++ fanya/homework.go
@@ -12,16 +12,18 @@ import (
 )
 
 const (
-	TODO = iota
-	FINISHED
-	EXPIRED
+	NONE_EXPIRED = -62135596800
+
+	TODO     = iota // 待做
+	FINISHED        // 已完成、待批阅
+	EXPIRED         // 已过期
 )
 
 // Homework 作业
 type Homework struct {
 	Title  string
-	Begin  int64
-	End    int64
+	Begin  time.Time
+	End    time.Time
 	Status int
 }
 
@@ -67,33 +69,34 @@ func (f *Fanya) GetHomeworks(course Courses) ([]Homework, error) {
 
 	homeworkBeginTimeNodes := htmlquery.Find(nodes, `//*[@id="RightCon"]/div/div/div[2]/ul/li/div[1]/span[1]/text()`)
 	for i, node := range homeworkBeginTimeNodes {
-		homeworks[i].Begin = 0
+		homeworks[i].Begin = time.Time{}
 
 		t, err := time.Parse("2006-01-02 15:04", node.Data)
 		if err != nil {
 			continue
 		}
-		homeworks[i].Begin = t.Unix()
+		homeworks[i].Begin = t
 	}
 
 	homeworkEndTimeNodes := htmlquery.Find(nodes, `//*[@id="RightCon"]/div/div/div[2]/ul/li/div[1]/span[2]/text()`)
 	for i, node := range homeworkEndTimeNodes {
-		homeworks[i].End = 0
+		homeworks[i].End = time.Time{}
 
-		t, err := time.Parse("2006-01-02 15:04", node.Data)
+		tz, _ := time.LoadLocation("Asia/Shanghai")
+		t, err := time.ParseInLocation("2006-01-02 15:04", node.Data, tz)
 		if err != nil {
 			continue
 		}
-		homeworks[i].End = t.Unix()
+		homeworks[i].End = t
 	}
 
-	homeworkStatusNodes := htmlquery.Find(nodes, `//*[@id="RightCon"]/div/div/div[2]/ul/li[1]/div[1]/span[3]/strong/text()`)
+	homeworkStatusNodes := htmlquery.Find(nodes, `//*[@id="RightCon"]/div/div/div[2]/ul/li/div[1]/span[3]/strong/text()`)
 	for i, node := range homeworkStatusNodes {
 		status := strings.TrimSpace(node.Data)
 		switch status {
 		case "待做":
 			homeworks[i].Status = TODO
-		case "待批阅":
+		case "待批阅", "已完成":
 			homeworks[i].Status = FINISHED
 		case "已过期":
 			homeworks[i].Status = EXPIRED
--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 
+	"github.com/imroc/req"
 	"github.com/wuhan005/hw-cron/cas"
 	"github.com/wuhan005/hw-cron/fanya"
 	log "unknwon.dev/clog/v2"
@@ -13,23 +18,67 @@ func init() {
 }
 
 func main() {
-	fanya := fanya.New()
-
-	cas, err := cas.NewSession("", "")
+	cas, err := cas.NewSession(os.Getenv("HDU_NO"), os.Getenv("HDU_PASSWORD"))
 	if err != nil {
 		log.Fatal("Failed to login: %v", err)
 	}
 
-	err = cas.ServiceLogin(fanya)
+	fy := fanya.New()
+	err = cas.ServiceLogin(fy)
 	if err != nil {
 		log.Fatal("Failed to login to fanya: %v", err)
 	}
 
-	courses, err := fanya.GetCourseList()
+	courses, err := fy.GetCourseList()
 	if err != nil {
 		log.Fatal("Failed to get courses list: %v", err)
 	}
 
-	fmt.Println(fanya.GetHomeworks(courses[2]))
+	for _, course := range courses {
+		homeworks, err := fy.GetHomeworks(course)
 
+		if err != nil {
+			log.Warn("Failed to get homework of %s: %v", course.Name, err)
+			continue
+		}
+		for _, hw := range homeworks {
+			if hw.Status == fanya.EXPIRED || hw.Status == fanya.FINISHED {
+				continue
+			}
+			timeToEnd := time.Duration(hw.End.UnixNano() - time.Now().UnixNano())
+
+			title := fmt.Sprintf("%s 作业即将截止", course.Name)
+			if timeToEnd < 0 {
+				content := fmt.Sprintf("%s - %s 需要提交，无截止日期。", course.Name, hw.Title)
+				sendAlert(title, content)
+				log.Trace(content)
+			} else if timeToEnd < 1*time.Hour { // < 1
+				content := fmt.Sprintf("%s - %s 还有不到 1 小时截止提交。【 %s 】", course.Name, hw.Title, hw.End.Format("2006-01-02 15:04:05"))
+				sendAlert(title, content)
+				log.Trace(content)
+			} else if timeToEnd < 3*time.Hour { // < 3
+				content := fmt.Sprintf("%s - %s 还有不到 3 小时截止提交。【 %s 】", course.Name, hw.Title, hw.End.Format("2006-01-02 15:04:05"))
+				sendAlert(title, content)
+				log.Trace(content)
+			} else if timeToEnd < 12*time.Hour { // < 12
+				content := fmt.Sprintf("%s - %s 还有不到 12 小时截止提交。【 %s 】", course.Name, hw.Title, hw.End.Format("2006-01-02 15:04:05"))
+				sendAlert(title, content)
+				log.Trace(content)
+			} else if timeToEnd < 24*time.Hour { // < 24
+				content := fmt.Sprintf("%s - %s 还有不到 24 小时截止提交。【 %s 】", course.Name, hw.Title, hw.End.Format("2006-01-02 15:04:05"))
+				sendAlert(title, content)
+				log.Trace(content)
+			}
+		}
+	}
+
+	log.Stop()
+}
+
+// sendAlert 发送推送，可以接入 Bark 等服务
+func sendAlert(title, content string) {
+	alertURL := os.Getenv("ALERT_URL")
+	alertURL = strings.ReplaceAll(alertURL, "{{title}}", url.QueryEscape(title))
+	alertURL = strings.ReplaceAll(alertURL, "{{content}}", url.QueryEscape(content))
+	_, _ = req.Get(alertURL)
 }
